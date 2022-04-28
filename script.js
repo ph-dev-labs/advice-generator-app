9f8a2359cff43693f18d3e16f1718c9dcb820edd
ph-dev-labs <nwosuchimezie95@gmail.com>
2022-04-28 11:27:57 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,33 @@
+var dice = document.querySelector('.dice');
+var speaker = document.querySelector(".advice-id");
+var advice = document.querySelector('.advice-text');
+
+Init()
+
+async function getResponse() {
+  try {
+    let response = await fetch("https://type.fit/api/quotes")
+    let data = await response.json();
+    dice.addEventListener("click", () => {
+    let randomGen = Math.floor(Math.random() * data.length); // generate random digits for generating random advice with authors
+    let text = data[randomGen].text;
+    let author = data[randomGen].author;
+    advice.textContent += text;
+    speaker.textContent += author;
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+getResponse()
+
+
+
+
+
+function Init () {
+  speaker.textContent = '';
+  advice.textContent = ''
+}
